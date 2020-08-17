885b16c53d5bb45818d4d3dc34e4e660a2770079
Jakob Selja <mail.jselja@gmail.com>
2020-08-17 15:54:21 +1000

    Add vector type parameter.
    Vector class now has IsCartesian property with additional constructor. Coordinates methods check if the vector is already of the type being converted to, to avoid potential errors of converting an already cartesian vector.

--- IsoMap/Coordinates.cs
+++ IsoMap/Coordinates.cs
@@ -4,33 +4,58 @@ namespace IsoMap
 {
     public class Vector2
     {
+        // Describes a point in 2D space, using X and Y.
+        // Two parameter constructor, automatically set to cartesian coordinates.
         public Vector2(int x, int y)
         {
             X = x;
             Y = y;
+            IsCartesian = true;
+        }
+
+        // Three parameter, constructor.
+        public Vector2(int x, int y, bool isCartesian)
+        {
+            X = x;
+            Y = y;
+            IsCartesian = isCartesian;
         }
 
         public int X { get; }
 
         public int Y { get; }
+
+        public bool IsCartesian { get; }
     }
 
     public class Coordinates
     {
         public Vector2 CartesianToIsometric(Vector2 v_in)
         {
-            int vx = v_in.X - v_in.Y;
-            int vy = (v_in.X + v_in.Y) / 2;
+            if (v_in.IsCartesian)
+            {
+                int vx = v_in.X - v_in.Y;
+                int vy = (v_in.X + v_in.Y) / 2;
 
-            return new Vector2(vx, vy);
+                return new Vector2(vx, vy, false);
+            } else
+            {
+                return v_in;
+            }
         }
 
         public Vector2 IsometricToCartesian(Vector2 v_in)
         {
-            int vx = (2 * v_in.Y + v_in.X) / 2;
-            int vy = (2 * v_in.Y - v_in.X) / 2;
+            if (!v_in.IsCartesian)
+            {
+                int vx = (2 * v_in.Y + v_in.X) / 2;
+                int vy = (2 * v_in.Y - v_in.X) / 2;
 
-            return new Vector2(vx, vy);
+                return new Vector2(vx, vy, true);
+            } else
+            {
+                return v_in;
+            }
         }
     }
 }
