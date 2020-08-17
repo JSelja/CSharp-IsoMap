f98fd6b1f55b34a6bd9f7457d91e828f00f4d925
Jakob Selja <mail.jselja@gmail.com>
2020-08-17 15:27:40 +1000

    Create coordinates class for vector conversions.

--- IsoMap/Class1.cs
+++
@@ -1,8 +0,0 @@
-﻿using System;
-
-namespace IsoMap
-{
-    public class Class1
-    {
-    }
-}
---
+++ IsoMap/Coordinates.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace IsoMap
+{
+    public class Vector2
+    {
+        public Vector2(int x, int y)
+        {
+            X = x;
+            Y = y;
+        }
+
+        public int X { get; }
+
+        public int Y { get; }
+    }
+
+    public class Coordinates
+    {
+        public Vector2 CartesianToIsometric(Vector2 v_in)
+        {
+            int vx = v_in.X - v_in.Y;
+            int vy = (v_in.X + v_in.Y) / 2;
+
+            return new Vector2(vx, vy);
+        }
+
+        public Vector2 IsometricToCartesian(Vector2 v_in)
+        {
+            int vx = (2 * v_in.Y + v_in.X) / 2;
+            int vy = (2 * v_in.Y - v_in.X) / 2;
+
+            return new Vector2(vx, vy);
+        }
+    }
+}
