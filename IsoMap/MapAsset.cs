845fd55d41cc315b99769fde5031ea58c47324d5
Jakob Selja <mail.jselja@gmail.com>
2020-08-18 16:29:08 +1000

    Add descriptive comments.

--- IsoMap/MapAsset.cs
+++ IsoMap/MapAsset.cs
@@ -7,21 +7,26 @@ namespace IsoMap
 {
     public static class MapAsset
     {
+        // The current map loaded in. Contains an instance of the Map class.
         public static Map ActiveMap { get; set; }
 
-
+        // Reads all information from a JSON file, and stores it as the active map.
         public static void LoadMap(string path)
         {
             string mapString;
 
+            // If the file is at the specified path,
             if (File.Exists(path))
             {
+                // Read it, and deserialize it to a new instance of the Map class.
                 mapString = File.ReadAllText(path);
                 ActiveMap = JsonSerializer.Deserialize<Map>(mapString, new JsonSerializerOptions { PropertyNameCaseInsensitive = true });
             }
+            // Sends a message if the map wasn't found, and loads a placeholder map to prevent runtime errors.
             else
             {
                 Console.Error.WriteLine("Map not found at " + path);
+                ActiveMap = new Map();
             }
         }
     }
